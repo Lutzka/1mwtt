b7cca0b9cab81ee14245d330474230f4901df2c7
Lutzka <41269789+Lutzka@users.noreply.github.com>
2018-07-25 10:17:47 +0200

    Create W1_Day3_favourite_number.py

---
+++ W1_Day3_favourite_number.py
@@ -0,0 +1,5 @@
+favourite_number = ""
+print("What's your favourite number?")
+print ("My favourite number is " + str(favourite_number) + ".")
+favourite_number = favourite_number + 1
+print ( str(favourite_number) + " is a bigger and a better number.")
