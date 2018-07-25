dd8e02707ea114e253b0c58b0e8bd4b1ffb519f6
Lutzka <41269789+Lutzka@users.noreply.github.com>
2018-07-25 10:22:50 +0200

    Create W1_Day3_greeting.py

---
+++ W1_Day3_greeting.py
@@ -0,0 +1,7 @@
+first_name = 'Lucie'
+middle_name = 'Kolkova'
+last_name = 'Vaculova'
+print("What's your first name?")
+print("what's your middle name?")
+print ("what's your last name?")
+print ('Hello ' + first_name + ' ' + middle_name + ' '+ last_name)
